efdbfc720379cb32621a733b72740f6860778d25
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-03-24 15:38:35 +0300

    BasePageBugFix

--- shop/urls.py
+++ shop/urls.py
@@ -3,13 +3,14 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import user_reviews 
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),  # Основной URL
+    path('home/', RedirectView.as_view(url='/')),  # Редирект с /home/ на /
     path('products/', views.products, name='products'),
-    path('reviews/', user_reviews, name='user_reviews'),
+    path('reviews/', views.user_reviews, name='user_reviews'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
